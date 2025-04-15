39b7932ef9b88d9073fe740318fe49981bd263a3
erikagaffney <erikagaffney.2014@gmail.com>
2025-04-15 10:20:41 -0500

    load card for card-badges capability

--- src/js/connector.js
+++ src/js/connector.js
@@ -3,6 +3,12 @@ console.log('Hello World');
 window.TrelloPowerUp.initialize({
   'card-badges': function(t, options) {
     // return an array of card badges for the card
-    return [];
+    return t.card("all").then(function (card) {
+      console.log(JSON.stringify(card, null, 2));
+      return [{
+        text: card.idShort,
+        color: 'green',
+      }];
+    });
   }
 });
