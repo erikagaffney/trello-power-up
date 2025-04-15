63da7f85f54cfeb01d7a470e6e62df3590f18dbf
erikagaffney <erikagaffney.2014@gmail.com>
2025-04-15 11:34:27 -0500

    testing out using the api

--- src/js/connector.js
+++ src/js/connector.js
@@ -2,29 +2,27 @@ console.log('Hello World');
 
 window.TrelloPowerUp.initialize({
   'card-badges': function (t, options) {
-    // return an array of card badges for the card
-    return t
-      .card('id')
-      .then(function (card) {
-        return t
-          .getRestApi()
-          .get(`/cards/${card.id}/actions`, { filter: 'updateCard:idList' });
-
-        //   console.log(JSON.stringify(card, null, 2));
-        //   return [{
-        //     text: card.idShort,
-        //     color: 'green',
-        //   }];
-        // });
-      })
-      .then(function (response) {
-        console.log('Response:', response);
-        return [
-          {
-            text: 'Hello World',
-            color: 'green',
-          },
-        ];
-      });
-  },
+    const restApi = t.getRestApi();
+    
+    return restApi.authorize().then(() => t.card('id'))
+    .then(card => {
+      return restApi.get('/cards/' + card.id + '/actions', { filter: 'updateCard:idList' });
+    }).then(actions => {
+      console.log('Actions:', actions);
+      return [{
+        text: 'Fetched!',
+        color: 'green'
+      }];
+    })
+    .catch(err => {
+      console.error('Error fetching card info or actions:', err);
+      return [{
+        text: 'Error',
+        color: 'red'
+      }];
+    });
+  }
+}, {
+  appKey: process.env.POWER_UP_TRELLO_API_KEY,
+  appName: 'Test Trello Power-Up',
 });
