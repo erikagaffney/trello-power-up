650ad42747062660d674adbf61e211b9df5c4949
erikagaffney <erikagaffney.2014@gmail.com>
2025-04-15 10:11:59 -0500

    added trello connector script and initial source code to connector

---
+++ src/js/connector.js
@@ -0,0 +1,8 @@
+console.log('Hello World');
+
+window.TrelloPowerUp.initialize({
+  'card-badges': function(t, options) {
+    // return an array of card badges for the card
+    return [];
+  }
+});
