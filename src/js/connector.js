04328926fa8924de2969fdf13b985820e42dff4d
erikagaffney <erikagaffney.2014@gmail.com>
2025-04-15 11:17:50 -0500

    testing out using the api

--- src/js/connector.js
+++ src/js/connector.js
@@ -1,14 +1,30 @@
 console.log('Hello World');
 
 window.TrelloPowerUp.initialize({
-  'card-badges': function(t, options) {
+  'card-badges': function (t, options) {
     // return an array of card badges for the card
-    return t.card("all").then(function (card) {
-      console.log(JSON.stringify(card, null, 2));
-      return [{
-        text: card.idShort,
-        color: 'green',
-      }];
-    });
-  }
+    return t
+      .card('id')
+      .then(function (card) {
+        return t
+          .getRestApi()
+          .get(`/cards/${card.id}/actions`, { filter: 'updateCard:idList' });
+
+        //   console.log(JSON.stringify(card, null, 2));
+        //   return [{
+        //     text: card.idShort,
+        //     color: 'green',
+        //   }];
+        // });
+      })
+      .then(function (response) {
+        console.log('Response:', response);
+        return [
+          {
+            text: 'Hello World',
+            color: 'green',
+          },
+        ];
+      });
+  },
 });
