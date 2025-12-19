b8c93ad2ce00e61d7fec36997031a33ba8d179f4
erikagaffney <erikagaffney.2014@gmail.com>
2025-12-19 15:28:57 -0600

    trying again for the sample-power-up

--- src/js/connector.js
+++ src/js/connector.js
@@ -2,27 +2,25 @@ console.log('Hello World');
 
 window.TrelloPowerUp.initialize({
   'card-badges': function (t, options) {
-    const restApi = t.getRestApi();
-    
-    return restApi.authorize().then(() => t.card('id'))
-    .then(card => {
-      return restApi.get('/cards/' + card.id + '/actions', { filter: 'updateCard:idList' });
-    }).then(actions => {
-      console.log('Actions:', actions);
+    // return array of card badges for the given card
+    console.log('card-badges called', t, options);
+    return t.card('all').then(function(card) {
+      console.log('card data', card);
       return [{
-        text: 'Fetched!',
-        color: 'green'
-      }];
-    })
-    .catch(err => {
-      console.error('Error fetching card info or actions:', err);
-      return [{
-        text: 'Error',
-        color: 'red'
+        title: 'Card ID',
+        text: card.idShort,
+        color: 'blue',
       }];
     });
   }
-}, {
-  appKey: process.env.POWER_UP_TRELLO_API_KEY,
-  appName: 'Test Trello Power-Up',
 });
+
+// TrelloPowerUp.initialize({
+//   'card-buttons': function(t, options){
+//     console.log("card-buttons called", t, options);
+//     return [{
+//       icon: 'https://cdn.glitch.com/1b42d7fe-bda8-4af8-a6c8-eff0cea9e08a%2Frocket-ship.png?1494946700421',
+//       text: 'Estimate Size',
+//     }];
+//   },
+// });
